f8d7707aae6f7b22801fa22a0f5e5f6c3825bcc8
Richard Trujillo <richard.trujillo.torres@gmail.com>
2019-09-14 18:21:11 +0200

    Rename record not found exception

--- app/Exceptions/RecordNotFoundException.php
+++ app/Exceptions/RecordNotFoundException.php
@@ -8,18 +8,18 @@ use Throwable;
  * Class RecordNotFound
  * @package App\Exceptions
  */
-class RecordNotFound extends \Exception
+class RecordNotFoundException extends \Exception
 {
     protected $message = 'Record not found';
 
     protected $code = 404;
 
-    public function __construct($resourceId = null, string $message = "", int $code = 0, Throwable $previous = null)
+    public function __construct($resourceId = null)
     {
         if ($resourceId) {
             $this->message = 'Record with ID ' . $resourceId . ' not found';
         }
 
-        parent::__construct($this->message, $code, $previous);
+        parent::__construct($this->message, $this->code);
     }
 }
