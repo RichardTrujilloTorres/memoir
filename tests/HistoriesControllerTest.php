468fc93353b8732ba4eacdb51b0bb3ef2ab8a4d8
Richard Trujillo <richard.trujillo.torres@gmail.com>
2019-09-15 00:24:14 +0200

    Add history controller test

--- tests/HistoriesControllerTest.php
+++ tests/HistoriesControllerTest.php
@@ -1,46 +1,118 @@
 <?php
 
-use Laravel\Lumen\Testing\DatabaseMigrations;
-use Laravel\Lumen\Testing\DatabaseTransactions;
-
-class HistoriesControlerTest extends TestCase
+/**
+ * Class HistoriesControllerTest
+ */
+class HistoriesControllerTest extends TestCase
 {
+    /**
+     * @var \App\History[] $records
+     */
     protected $records;
 
     protected function setUp(): void
     {
-        $this->records = factory(\App\History::class)->make();
+        parent::setUp();
+
+        \Illuminate\Support\Facades\DB::table('histories')->truncate();
+
+        $this->records = factory(\App\History::class, 10)->create();
     }
 
 
 
     /**
-     * @test
+     * @test History record listing.
      */
     public function index()
     {
         $response = $this->call('GET', '/histories');
 
         $this->assertEquals(200, $response->status());
-        
+
         $this->seeJson([
             '_id' => $this->records[0]->_id
         ]);
     }
 
     /**
-     * @test
+     * @test History record detail.
      */
     public function show()
     {
-        $record = factory(\App\History::class)->create();
-
-        $response = $this->call('GET', "histories/{$record->_id}");
+        $response = $this->call('GET', "histories/{$this->records[0]->_id}");
 
         $this->assertEquals(200, $response->status());
 
         $this->seeJson([
-            '_id' => $record->_id,
+            'contact' => $this->records[0]->contact,
+            '_id' => $this->records[0]->_id,
+        ]);
+    }
+
+    /**
+     * @test History record creation.
+     */
+    public function store()
+    {
+        $response = $this->call('POST', "histories", []);
+
+        $this->assertEquals(422, $response->status());
+
+        $response = $this->call('POST', "histories", [
+            'contact' => '/api/v1/contacts/11',
+            'type' => 'purchase made',
+            'followUp' => 'thank you email',
+            'links' => [
+                '/api/v1/products/1',
+            ],
+        ]);
+
+        $this->assertEquals(201, $response->status());
+
+        $this->seeJson([
+            'contact' => '/api/v1/contacts/11',
+            'type' => 'purchase made',
+        ]);
+
+        $this->seeInDatabase('histories', [
+            'contact' => '/api/v1/contacts/11',
+            'type' => 'purchase made',
+        ]);
+    }
+
+    /**
+     * @test History record update.
+     */
+    public function update()
+    {
+        $response = $this->call('PUT', "histories/{$this->records[0]->_id}", [
+            'type' => 'purchase made (--updated)',
+        ]);
+
+        $this->assertEquals(201, $response->status());
+
+        $this->seeJson([
+            'type' => 'purchase made (--updated)',
+        ]);
+
+        $this->seeInDatabase('histories', [
+            'type' => 'purchase made (--updated)',
+        ]);
+    }
+
+
+    /**
+     * @test History record removal.
+     */
+    public function deleteAction()
+    {
+        $response = $this->call('DELETE', "histories/{$this->records[0]->_id}");
+
+        $this->assertEquals(201, $response->status());
+
+        $this->seeJson([
+            'data' => [],
         ]);
     }
 }
